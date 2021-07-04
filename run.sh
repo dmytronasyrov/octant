c684c05cd1395b08cb35899be444ae4a0e267c45
Dmytro Nasyrov <dmytro@pharosproduction.com>
2021-07-05 00:18:16 +0300

    remove run

--- run.sh
+++
@@ -1,6 +0,0 @@
-docker run -d \
-  --name="octant" \
-  -p 8000:8000 \
-  -v "${HOME}/.kube/config:/kube/config" \
-  -v "/Users/rudra/.minikube/profiles/localdev/:/Users/rudra/.minikube/profiles/localdev/" \
-  dmytronasyrov/octant:latest
